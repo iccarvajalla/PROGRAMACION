afaa0840faaa5962a75e0a87ba7a728610ff3308
iccarvajall <iccarvajall@unal.edu.co>
2017-02-12 22:50:23 -0800

    esto es la primera modificacion

---
+++ alfa.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main (void)
+{
+  string mystring;
+  mystring = "sto es un string";
+  std::cout << mystring;
+  mystring = "hola italo";
+  std::cout << mystring;
+
+  return 0;
+}    
