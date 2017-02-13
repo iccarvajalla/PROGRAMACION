e747bc256aa3c3945e885204a4ecd78543903814
iccarvajall <iccarvajall@unal.edu.co>
2017-02-13 11:07:52 -0500

    cro que estoy aprendiendo :v

---
+++ delta.cpp
@@ -0,0 +1,17 @@
+//declaracion de variables
+
+#include <iostream>
+
+int main(void)
+{
+  double d = 2.2;
+  double i = 7;
+  d = d + i;
+  i = d*i;
+
+  std::cout << d << std::endl;
+  std::cout << i << std::endl;
+
+  return 0;
+
+}
