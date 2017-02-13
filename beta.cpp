db83aa651c9149cd63b9a355e54b6aea4ebd00a3
iccarvajall <iccarvajall@unal.edu.co>
2017-02-13 10:38:56 -0500

    esto es otro experimento

---
+++ beta.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+int main(void)
+{
+
+
+  //codigo para le claculo de la raiz cuadrada
+
+  double raiz2 = sqrt(2);
+
+  std::cout << raiz2 << std::endl;
+
+  return 0;
+
+} 
