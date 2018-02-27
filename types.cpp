636ea881107976f7aa04360980ad7e6b42c51915
Italo Carvajal Lavi <iccarvajall@unal.edu.co>
2018-02-27 08:19:04 -0800

    ejercicios en casa: datos

---
+++ types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+
+{
+  std::cout << " tipo de datos bytes" << std::endl;
+  std::cout <<"-------------------" << std::endl;
+  std::cout << "int" << sizeof(int) << std::endl;
+  std::cout << "char" << sizeof(char) << std::endl;
+  std::cout << "bool" << sizeof(bool) << std::endl;
+
+  return 0;
+
+}
