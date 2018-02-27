395e2651f3f607c807d079834449b083d45ec38a
Italo Carvajal Lavi <iccarvajall@unal.edu.co>
2018-02-27 08:59:46 -0800

    actualizacion types.cpp: segundo commit

--- types.cpp
+++ types.cpp
@@ -4,10 +4,10 @@ int main()
 
 {
   std::cout << " tipo de datos bytes" << std::endl;
-  std::cout <<"-------------------" << std::endl;
-  std::cout << "int" << sizeof(int) << std::endl;
-  std::cout << "char" << sizeof(char) << std::endl;
-  std::cout << "bool" << sizeof(bool) << std::endl;
+  std::cout << "------------  -------" << std::endl;
+  std::cout << "int            " << sizeof(int) << std::endl;
+  std::cout << "char           " << sizeof(char) << std::endl;
+  std::cout << "bool           " << sizeof(bool) << std::endl;
 
   return 0;
 
