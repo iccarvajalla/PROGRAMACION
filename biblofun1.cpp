295d81e3db1b72470004a900155a980a98b4e8b9
Italo Carvajal Lavi <iccarvajall@unal.edu.co>
2018-03-01 23:25:04 -0800

    ejercicio casa biblioteca matematica

---
+++ biblofun1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+
+
+  sin a;
+  cos b;
+  tan c;
+{
+
+  int main()
+    
+  std::cin >> a;
+  std::cin >> b;
+  std::cin >> c;
+
+  std::cout << " sen de" << a << "es:" << std::endl;
+  std::cout << " cos de" << b << "es:" << std::endl;
+  std::cout << " tan de" << c << "es:" << std::endl;
+
+  return 0;
+} 
+    
