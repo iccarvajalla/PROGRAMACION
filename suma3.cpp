e25152e3c027bb761979abfd953a2dcbce565f39
Italo Crover Carvaja Lavi <iccarvajall@unal.edu.co>
2018-03-14 08:32:24 -0500

    ejercicio fallido do-while xd: leer link

---
+++ suma3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+// https://www.programarya.com/Cursos/C++/Ciclos/Ciclo-do-while
+double sumdowh(int N)
+{
+  int n = 0, sum = 0;
+  do(n = 1; n <= N; n++) {
+
+    sum += n;
+  }
+
+  return sum;
+}
+
+int main(void)
+{
+  std::cout <<  << std::endl;
+ 
+  
+  return 0;
+}
