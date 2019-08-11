dbd967d4f718b98b88494b8c6fb4a5142430325e
crover <itccarvajalla@unal.edu.co>
2019-08-10 21:45:48 -0500

    calcula el promedio de 7 datos

---
+++ promedio.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+
+{
+  double a, b, c, d, e, f, g, x;
+  x = (a+b+c+d+e+f+g)/7;
+  std::cout << "ingrese los nueve valores " << std::endl;
+  std::cin >> a;
+  std::endl;
+  std::cin >> b;
+  std::endl;
+  std::cin >> c;
+  std::endl;
+  std::cin >> d;
+  std::endl;
+  std::cin >> e;
+  std::endl;
+  std::cin >> f;
+  std::endl;
+  std::cin >> g;
+  std::endl;
+
+  std::cout << " el promedio es: " << std::cout <<  x;
+
+  std::endl;
+
+  return 0;
+
+}
